609040b1a21f9db08d23465a4f27d0f328dbcb9e
xHx-Alan <123024012+xHx-Alan@users.noreply.github.com>
2023-11-24 15:38:39 -0300

    refactoring

--- src/index.js
+++ src/index.js
@@ -7,6 +7,7 @@ app.use(bodyParser.json());
 
 const users = [];
 
+// Criar conta
 app.post('/criar-conta', (req, res) => {
     const { name, email, password } = req.body;
 
@@ -19,12 +20,14 @@ app.post('/criar-conta', (req, res) => {
         name,
         email,
         password,
+        messages: []
     };
 
     users.push(newUser);
     res.json(newUser);
 });
 
+// Login
 app.post('/login', (req, res) => {
     const { email, password } = req.body;
 
@@ -38,4 +41,70 @@ app.post('/login', (req, res) => {
 });
 
 
+app.get("/usuarios", (request, response) => {
+    return response.json(users);
+});
+
+// Adicionar novo recado
+app.post('/novorecado/:userId', (req, res) => {
+    const { userId } = req.params;
+    const { messageContent } = req.body;
+
+    const user = users.find(user => user.id === parseInt(userId));
+
+    if (!user) {
+        return res.status(404).json({ error: 'Usuário não encontrado' });
+    }
+
+    const newMessage = {
+        id: user.messages.length + 1,
+        content: messageContent,
+    };
+
+    user.messages.push(newMessage);
+    res.json({ message: 'Recado adicionado com sucesso', newMessage });
+});
+
+// Editar recado
+app.put('/editarecado/:userId/:messageId', (req, res) => {
+    const { userId, messageId } = req.params;
+    const { messageContent } = req.body;
+
+    const user = users.find(user => user.id === parseInt(userId));
+
+    if (!user) {
+        return res.status(404).json({ error: 'Usuário não encontrado' });
+    }
+
+    const messageToUpdate = user.messages.find(message => message.id === parseInt(messageId));
+
+    if (!messageToUpdate) {
+        return res.status(404).json({ error: 'Recado não encontrado' });
+    }
+
+    messageToUpdate.content = messageContent;
+    res.json({ message: 'Recado atualizado com sucesso', updatedMessage: messageToUpdate });
+});
+
+// Apagar recado
+app.delete('/delrecado/:userId/:messageId', (req, res) => {
+    const { userId, messageId } = req.params;
+
+    const user = users.find(user => user.id === parseInt(userId));
+
+    if (!user) {
+        return res.status(404).json({ error: 'Usuário não encontrado' });
+    }
+
+    const messageIndex = user.messages.findIndex(message => message.id === parseInt(messageId));
+
+    if (messageIndex === -1) {
+        return res.status(404).json({ error: 'Recado não encontrado' });
+    }
+
+    user.messages.splice(messageIndex, 1);
+    res.json({ message: 'Recado apagado com sucesso' });
+});
+
+// Iniciar servidor
 app.listen(8080, () => console.log("Servidor iniciado"));
